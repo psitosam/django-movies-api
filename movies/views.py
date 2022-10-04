1411d1d5b91aef860a720262f3a0e1f75d8c92c9
Alex Psitos <apsitos08@gmail.com>
2022-10-04 15:29:30 -0400

    Feat: show page functionality built into links on index page

--- movies/views.py
+++ movies/views.py
@@ -25,7 +25,11 @@ from .models import Movie
 
 def movies(request):
   data = Movie.objects.all()
-  return render(request, 'movies/movies.html', {'data': data})
+  return render(request, 'movies/movies.html', {'movies': data})
 
 def home(request):
     return HttpResponse("Home page")
+
+def detail(request, id):
+  data = Movie.objects.get(pk=id)
+  return render(request, 'movies/detail.html', {'movie': data})
